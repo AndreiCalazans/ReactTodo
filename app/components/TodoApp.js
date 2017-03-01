a5adfc56d6976a356556b865fb80bb9e7a3c54bd
AndreiCalazans <andreixoc@hotmail.com>
2017-03-01 18:56:01 -0300

    made todo search input

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import TodoList from 'TodoList';
 import TodoAdder from 'TodoAdder'
+import TodoSearch from 'TodoSearch';
+
+
 var TodoApp = React.createClass({
   getInitialState: function() {
     return {
+      showCompleted: false,
+      searchText: '',
       todos: [
         {
           id:1,
@@ -32,11 +37,18 @@ var TodoApp = React.createClass({
       text: text
     });
   },
+  handleSearch: function( showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function() {
     var {todos} = this.state;
 
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}></TodoSearch>
         <TodoList todos={todos}></TodoList>
         <TodoAdder onAddTodo={this.handleAddTodo}></TodoAdder>
       </div>
