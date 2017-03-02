7aec89707d95f353ebd0b45ece7f7a3926d604f7
AndreiCalazans <andreixoc@hotmail.com>
2017-03-02 06:55:36 -0300

    made todo filters and sorts and test for it

--- app/api/TodoAPI.js
+++ app/api/TodoAPI.js
@@ -18,8 +18,34 @@ module.exports = {
     } catch (e) {
 
     }
-    // intenarary operator below
+    // intenary operator below
   return $.isArray(todos) ? todos : [] ;
 
-  }
+},
+filterTodos: function(todos, showCompleted, searchText) {
+  var filteredTodos = todos;
+
+  //fillter by show completed
+  filteredTodos = filteredTodos.filter((todo) => {
+    return !todo.completed || showCompleted;
+  });
+
+  //filter by searchText
+  filteredTodos = filteredTodos.filter((todo) => {
+    var text = todo.text.toLowerCase();
+    return searchText.length === 0 || text.indexOf(searchText) > -1;
+  });
+
+  //sort todos with non-completed first
+  filteredTodos.sort((a ,b) => {
+    if ( !a.completed && b.completed ){
+      return -1;
+    } else if (a.completed && !b.completed) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  return filteredTodos;
+}
 };
--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -46,12 +46,13 @@ var TodoApp = React.createClass({
     });
   },
   render: function() {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos , showCompleted, searchText);
 
-    return (
+      return (
       <div>
         <TodoSearch onSearch={this.handleSearch}></TodoSearch>
-        <TodoList todos={todos} onToggle={this.handleToggle}></TodoList>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}></TodoList>
         <TodoAdder onAddTodo={this.handleAddTodo}></TodoAdder>
       </div>
     )
