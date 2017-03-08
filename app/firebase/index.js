071a578c97a4022ee471e99d75f9a61959f33d4c
AndreiCalazans <andreixoc@hotmail.com>
2017-03-08 06:56:02 -0300

    moved firebase creds to a env file

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,11 +3,11 @@ import firebase from 'firebase';
 try {
   //CRUD creating , reading , updating , deleting
   var config = {
-    apiKey: "AIzaSyDUC-EjPfOI-FtqRc-9U5BI_wh7OPe5B3o",
-    authDomain: "andrei-todo-app.firebaseapp.com",
-    databaseURL: "https://andrei-todo-app.firebaseio.com",
-    storageBucket: "andrei-todo-app.appspot.com",
-    messagingSenderId: "1032996165232"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
